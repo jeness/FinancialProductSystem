4b2d21c5f3f1407c0e02ce0deca5da2c5832cebd
jeness <yuhaoran9968@gmail.com>
2019-10-03 22:56:35 -0400

    restructure

--- management-center-3.8.4/updateLdapPassword.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $# -gt 0 ] ; then
-    echo "usage: updateLdapPassword.sh"
-    exit;
-fi
-
-java -cp mancenter-3.8.4.war Launcher updateLdapPassword
--- management-center-3.8.4/work/jetty-0.0.0.0-8080-mancenter-3.8.4.war-_mancenter-any-/webapp/javascripts/app/docs.js
+++
@@ -1,3 +0,0 @@
-// don't delete, this method is called dynamically by Tabs.prototype.showTab on tab.js
-function docsInit()
-{}
